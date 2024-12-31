f929063359e3bae25824545f92bea9c247a5e56c
SY17SY <88537310+SY17SY@users.noreply.github.com>
2024-12-31 16:52:34 +0900

    1231 Fix to use Local

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql://scutta_user:xNkGLWfqC21ICB8SEI5kXU6FTW1dbtnU@dpg-ctpi2ja3esus73di7r3g-a.singapore-postgres.render.com/scutta_2025_1"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
