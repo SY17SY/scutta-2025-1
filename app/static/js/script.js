7aa52cb235f29d28f265ab751a71e542713a77d9
SY17SY <88537310+SY17SY@users.noreply.github.com>
2025-01-01 01:32:00 +0900

    feat focusin close keyboard

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,26 @@
+let inputFocused = false;
+
+document.addEventListener('focusin', (event) => {
+    if (event.target.tagName === 'INPUT') {
+        inputFocused = true;
+        document.addEventListener('click', handleOutsideClick);
+    }
+});
+
+document.addEventListener('focusout', (event) => {
+    if (event.target.tagName === 'INPUT') {
+        inputFocused = false;
+        document.removeEventListener('click', handleOutsideClick);
+    }
+});
+
+function handleOutsideClick(event) {
+    const inputs = document.querySelectorAll('input');
+    if (!event.target.closest('input') && inputFocused) {
+        inputs.forEach(input => input.blur());
+    }
+}
+
 function navigateTo(page) {
     window.location.href = page;
 }
