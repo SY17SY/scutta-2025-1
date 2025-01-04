77414baca75f26349bda82a0e40f2818d3245fbe
SY17SY <88537310+SY17SY@users.noreply.github.com>
2025-01-04 16:57:32 +0900

    re fix health_check

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 import os
+import signal
+import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,6 +9,10 @@ from config import Config
 db = SQLAlchemy()
 
 migrate = Migrate()
+
+def handle_exit_signal(signum, frame):
+    print("Shutting down gracefully...")
+    sys.exit(0)
 
 def create_app():
         
@@ -18,5 +24,8 @@ def create_app():
     with app.app_context():
         from . import routes, models
         db.create_all()
+    
+    signal.signal(signal.SIGTERM, handle_exit_signal)
+    signal.signal(signal.SIGINT, handle_exit_signal)
 
     return app
