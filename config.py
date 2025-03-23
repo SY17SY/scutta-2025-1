15a4eb429f9021ec1911222ae9b59f85960ef4df
SY17SY <88537310+SY17SY@users.noreply.github.com>
2025-03-23 21:29:21 +0900

    fix config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 from urllib.parse import urlparse
 
 class Config:
-    # db_url = os.getenv("DATABASE_URL")
-    #
-    # parsed_url = urlparse(db_url)
-    #
-    # if parsed_url.hostname and "singapore-postgres.render.com" in parsed_url.hostname:
-    #     db_url += "?sslmode=require"
-    # else:
-    #     db_url += "?sslmode=disable"
+    db_url = os.getenv("DATABASE_URL")
+    
+    parsed_url = urlparse(db_url)
+    
+    if parsed_url.hostname and "singapore-postgres.render.com" in parsed_url.hostname:
+        db_url += "?sslmode=require"
+    else:
+        db_url += "?sslmode=disable"
 
     SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
